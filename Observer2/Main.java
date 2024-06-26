a1a045f20c7782f6363e9b9221fb8471f762c509
fsRakib <fsrakiba50@gmail.com>
2024-06-26 19:41:23 +0600

    Auto-sync commit

--- Observer2/Main.java
+++ Observer2/Main.java
@@ -4,11 +4,20 @@ public class Main {
     public static void main(String[] args) {
 
         YoutubeChannel channel = new YoutubeChannel("fsNews");
+        YoutubeChannel channel2 = new YoutubeChannel("fsNews");
 
         User user1 = new User("Rakib");
         User user2 = new User("Arka");
         User user3 = new User("Siam");
 
-        channel
+        channel.subscribe(user1);
+        channel.subscribe(user2);
+        channel.subscribe(user3);
+
+        channel.uploadVideo("Static NAT");
+
+        System.out.println("==============================");
+        channel.unsubscribe(user3);
+        channel.uploadVideo("Dynamic NAT");
     }
 }
--- Observer2/User.java
+++ Observer2/User.java
@@ -10,7 +10,7 @@ public class User implements Subscriber {
 
     @Override
     public void update(String channelName, String videoName) {
-        System.out.println("Hi "+username+", a new video "+videoName+"has been uploaded to "+channelName+"channel.");
+        System.out.println("Hi "+username+", a new video ("+videoName+") has been uploaded to "+channelName+"channel.");
 
     }
 
