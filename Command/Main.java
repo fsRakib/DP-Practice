30eef294bcd82f928a125eb7fa56246296c8198b
fsRakib <fsrakiba50@gmail.com>
2024-06-26 17:46:23 +0600

    Auto-sync commit

--- Command/Main.java
+++ Command/Main.java
@@ -3,7 +3,7 @@ package Command;
 public class Main {
     public static void main(String[] args) {
         FileOperationExecutor fileOperationExecutor = new FileOperationExecutor();
-        fileOperationExecutor.executeOperation(new OpenFileOperation(new TextFile("file1.txt")));
+        fileOperationExecutor.executeOperation(new OpenFile(new TextFile("file1.txt")));
         fileOperationExecutor.executeOperation(new SaveFile(new TextFile("file2.txt")));
     }
 }
