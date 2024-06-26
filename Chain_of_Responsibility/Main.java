864cdd4875df12d46838a952da0dd8a3d82514cd
fsRakib <fsrakiba50@gmail.com>
2024-06-26 13:56:23 +0600

    Auto-sync commit

--- Chain_of_Responsibility/Main.java
+++ Chain_of_Responsibility/Main.java
@@ -2,18 +2,16 @@ package Chain_of_Responsibility;
 
 public class Main {
     public static void main(String[] args) {
-        PaymentHandler bank = new BankPaymentHandler();
-        PaymentHandler card = new CreditCardPaymentHandler();
-        PaymentHandler paypal = new PayPalPaymentHandler();
+        PaymentHandler bank= new BankPaymentHandler();
+        PaymentHandler card= new CreditCardPaymentHandler();
+        PaymentHandler paypal= new PayPalPaymentHandler();
 
         bank.setNext(card);
         card.setNext(paypal);
 
         bank.handlePayment(100);
-        bank.handlePayment(500);
-        bank.handlePayment(1200);
         bank.handlePayment(600);
-        bank.handlePayment(2400);
+        bank.handlePayment(1200);
         card.handlePayment(600);
         card.handlePayment(1200);
         paypal.handlePayment(200);
