4f8335858c19f07d4458d558f2c8639b80971abe
fsRakib <fsrakiba50@gmail.com>
2024-06-26 15:26:23 +0600

    Auto-sync commit

--- Command/FileOperationExecutor.java
+++ Command/FileOperationExecutor.java
@@ -1,5 +1,14 @@
 package Command;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileOperationExecutor {
-    
+    private final List<FileOperation> fileOperations = new ArrayList<>();
+
+    public String executeOperation(FileOperation fileOperation) {
+        fileOperations.add(fileOperation);
+        return fileOperation.execute();
+
+    }
 }
