7d3d5d8374db4e03eab99634483f7d8dd19ffd0f
fsRakib <fsrakiba50@gmail.com>
2024-06-26 18:16:23 +0600

    Auto-sync commit

---
+++ Observer/NewsChannel.java
@@ -0,0 +1,18 @@
+package Observer;
+
+public class NewsChannel implements Channel{
+private String news;
+    @Override
+    public void update(Object o) {
+        this.setNews((String)news);
+        
+    }
+    public String getNews() {
+        return news;
+    }
+    public void setNews(String news) {
+        this.news = news;
+    }
+    
+    
+}
