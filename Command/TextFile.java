811a63d7b962b8d6746ae231cc494604ca119ef2
fsRakib <fsrakiba50@gmail.com>
2024-06-26 15:16:23 +0600

    Auto-sync commit

--- Command/TextFile.java
+++ Command/TextFile.java
@@ -6,7 +6,12 @@ public class TextFile {
     public TextFile(String name) {
         this.name = name;
     }
-    public String open(){
-        return "Opening file"+ 
+
+    public String open() {
+        return "Opening file" + name;
+    }
+
+    public String save() {
+        return "Saving file" + name;
     }
 }
