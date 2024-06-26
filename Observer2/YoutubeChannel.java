878f0b66b83951e7f655d0c3c86378e96c2e42dc
fsRakib <fsrakiba50@gmail.com>
2024-06-26 19:26:23 +0600

    Auto-sync commit

--- Observer2/YoutubeChannel.java
+++ Observer2/YoutubeChannel.java
@@ -4,25 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class YoutubeChannel implements Channel {
-    private List<Subscriber>subscribers= new ArrayList<>();
+    private List<Subscriber> subscribers = new ArrayList<>();
+    private String channeLName;
+
+    public YoutubeChannel(String channeLName) {
+        this.channeLName = channeLName;
+    }
 
     @Override
     public void notifySubscribers(String videoname) {
-        // TODO Auto-generated method stub
         
+
     }
 
     @Override
     public void subscribe(Subscriber subscriber) {
-        // TODO Auto-generated method stub
-        
+        subscribers.add(subscriber);
+
     }
 
     @Override
     public void unsubscribe(Subscriber subscriber) {
-        // TODO Auto-generated method stub
-        
+        subscribers.remove(subscriber);
+
     }
-    
-    
+
 }
