a441ebaf10ed4f020490bdbfb79d37ae2e33ed52
fsRakib <fsrakiba50@gmail.com>
2024-06-26 14:56:23 +0600

    Auto-sync commit

---
+++ Command/OpenFileOperation.java
@@ -0,0 +1,11 @@
+package Command;
+
+public class OpenFileOperation implements FileOperation {
+ private TextFile textFile;
+    @Override
+    public String execute() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
