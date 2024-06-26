22b080225ea02c22c54ed433d41d50cd4abb99e6
fsRakib <fsrakiba50@gmail.com>
2024-06-26 12:43:25 +0600

    Chain Of Responsibility

---
+++ Chain_of_Responsibility/BankPaymentHandler.java
@@ -0,0 +1,16 @@
+package Chain_of_Responsibility;
+
+public class BankPaymentHandler extends PaymentHandler {
+
+    @Override
+    public void handlePayment(double amount) {
+        // TODO Auto-generated method stub
+        if (amount < 500) {
+            System.out.println("Payment of " + amount + " is handled by BankPaymentHandler");
+        } else {
+            next.handlePayment(amount);
+        }
+
+    }
+
+}
---
+++ Chain_of_Responsibility/Main.java
@@ -0,0 +1,20 @@
+package Chain_of_Responsibility;
+
+public class Main {
+    public static void main(String[] args) {
+        PaymentHandler bank= new BankPaymentHandler();
+        PaymentHandler card= new CreditCardPaymentHandler();
+        PaymentHandler paypal= new PayPalPaymentHandler();
+
+        bank.setNext(card);
+        card.setNext(paypal);
+
+        bank.handlePayment(100);
+        bank.handlePayment(600);
+        bank.handlePayment(1200);
+        card.handlePayment(600);
+        card.handlePayment(1200);
+        paypal.handlePayment(200);
+        paypal.handlePayment(1499);
+    }
+}
