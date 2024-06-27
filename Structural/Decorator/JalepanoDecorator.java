93e14509fa0033370d6f8d5b8856c93af7a471c1
fsRakib <fsrakiba50@gmail.com>
2024-06-28 00:31:23 +0600

    Auto-sync commit

--- Structural/Decorator/JalepanoDecorator.java
+++ Structural/Decorator/JalepanoDecorator.java
@@ -1,5 +1,17 @@
 package Structural.Decorator;
 
-public class JalepanoDecorator extends PizzaDecorator{
-    
+public class JalepanoDecorator extends PizzaDecorator {
+
+    public JalepanoDecorator(Pizza pizza) {
+        super(pizza);
+    }
+
+    @Override
+    public String bake() {
+        return super.bake() + addJalepano();
+    }
+
+    public String addJalepano() {
+        return "Jalepano";
+    }
 }
