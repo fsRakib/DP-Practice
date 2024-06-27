aa16634a5aecc7bbd281dd773506702e3a2c15d7
fsRakib <fsrakiba50@gmail.com>
2024-06-28 04:47:12 +0600

    Auto-sync commit

--- Structural/Composite/HeadDept.java
+++ Structural/Composite/HeadDept.java
@@ -1,13 +1,32 @@
 package Structural.Composite;
 
-public class HeadDept implements Department{
+import java.util.ArrayList;
+import java.util.List;
+
+//composite part
+public class HeadDept implements Department {
     private Integer id;
     private String name;
-    
+    private List<Department> childDepartments;
+
+    public HeadDept(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+        this.childDepartments = new ArrayList<>();
+    }
+
     @Override
     public void printDeptName() {
         // TODO Auto-generated method stub
-        
+
     }
-    
+
+    public void addDept(Department department) {
+        childDepartments.add(department);
+    }
+
+    public void removeDept(Department department) {
+        childDepartments.remove(department);
+    }
+
 }
