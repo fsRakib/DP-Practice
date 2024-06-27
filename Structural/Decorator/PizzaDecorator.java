e502ac30e980cba821dced494e10a4287a8a7eed
fsRakib <fsrakiba50@gmail.com>
2024-06-28 00:16:23 +0600

    Auto-sync commit

---
+++ Structural/Decorator/PizzaDecorator.java
@@ -0,0 +1,11 @@
+package Structural.Decorator;
+
+public class PizzaDecorator implements Pizza{
+
+    @Override
+    public String bake() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
