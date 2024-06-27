60fe7189ce252ecd26b7dc30e41bbaafc6608b9b
fsRakib <fsrakiba50@gmail.com>
2024-06-27 15:31:23 +0600

    Auto-sync commit

--- Creational/Singleton/Main.java
+++ Creational/Singleton/Main.java
@@ -2,9 +2,11 @@ package Creational.Singleton;
 
 public class Main {
     public static void main(String[] args) {
-        Samosa samosa1 =  Samosa.getSamosa();
+        Samosa samosa1 = Samosa.getSamosa();
         System.out.println(samosa1.hashCode());
 
+        Samosa samosa2 = Samosa.getSamosa();
+        System.out.println(samosa2.hashCode());
 
     }
 }
