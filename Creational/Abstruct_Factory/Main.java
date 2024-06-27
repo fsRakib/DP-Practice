c78b570034c2c9daa1ed68f82dde3406d138f9f4
fsRakib <fsrakiba50@gmail.com>
2024-06-27 20:17:12 +0600

    Auto-sync commit

--- Creational/Abstruct_Factory/Main.java
+++ Creational/Abstruct_Factory/Main.java
@@ -5,5 +5,8 @@ public class Main {
         Employee e1 = Factory.getEmployee(new AndroidDevFactory());
         e1.name();
         System.out.println("Salary: "+e1.salary());
+
+        Employee e2= Factory.getEmployee(new WebDevFactory());
+        e2.name();
     }
 }
---
+++ Creational/Abstruct_Factory/Manager.java
@@ -0,0 +1,17 @@
+package Creational.Abstruct_Factory;
+
+public class Manager implements Employee{
+
+    @Override
+    public String name() {
+        
+        return "Mana";
+    }
+
+    @Override
+    public int salary() {
+        // TODO Auto-generated method stub
+        return 100000;
+    }
+    
+}
