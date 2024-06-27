56fa49c393e2a61b390430fcdebd234d403bf9e4
fsRakib <fsrakiba50@gmail.com>
2024-06-27 21:07:13 +0600

    Auto-sync commit

---
+++ Creational/Builder/User.java
@@ -0,0 +1,19 @@
+package Creational.Builder;
+
+public class User {
+    private final String username;
+    private final String userId;
+    private final String address;
+    private User() {
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public String getAddress() {
+        return address;
+    }
+    
+}
