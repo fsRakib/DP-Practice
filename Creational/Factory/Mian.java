f27d0e2aa005113bd602444bb0a5250c6bcb45a0
fsRakib <fsrakiba50@gmail.com>
2024-06-27 17:01:23 +0600

    Auto-sync commit

--- Creational/Factory/Mian.java
+++ Creational/Factory/Mian.java
@@ -2,6 +2,9 @@ package Creational.Factory;
 
 public class Mian {
     public static void main(String[] args) {
-        Employee 
+        Employee employee = Factory.getEmployee("android");
+        System.out.println(employee);
+        int salary = employee.salary();
+        System.out.println("Salary: " + salary);
     }
 }
