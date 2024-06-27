c46fcb063864143eea735f0872b6a082a6984fc1
fsRakib <fsrakiba50@gmail.com>
2024-06-27 17:57:13 +0600

    Auto-sync commit

---
+++ Creational/Abstruct_Factory/AbstructFactory.java
@@ -0,0 +1,8 @@
+package Creational.Abstruct_Factory;
+
+abstract public class AbstructFactory {
+    //create employee
+    public Employee creatEmployee(){
+        
+    }
+}
--- Creational/Abstruct_Factory/Factory.java
+++ Creational/Abstruct_Factory/Factory.java
@@ -2,5 +2,7 @@ package Creational.Abstruct_Factory;
 
 public class Factory {
     //get employee
-    
+    public static Employee getEmployee(){
+        
+    }
 }
