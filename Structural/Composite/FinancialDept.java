32060e68f2e5df16db460e87995fc5ca39cd37e6
fsRakib <fsrakiba50@gmail.com>
2024-06-28 04:37:12 +0600

    Auto-sync commit

---
+++ Structural/Composite/FinancialDept.java
@@ -0,0 +1,12 @@
+package Structural.Composite;
+//leaf component
+public class FinancialDept implements Department{
+    private Integer id;
+    private Str
+    @Override
+    public void printDeptName() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
