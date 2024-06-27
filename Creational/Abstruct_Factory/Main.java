e45024045d159e0ddc652947faac9fa11868376c
fsRakib <fsrakiba50@gmail.com>
2024-06-27 20:12:12 +0600

    Auto-sync commit

--- Creational/Abstruct_Factory/Main.java
+++ Creational/Abstruct_Factory/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Employee e1 = Factory.getEmployee(new AndroidDevFactory());
         e1.name();
-        System.out.println("Salary: "e1.salary());
+        System.out.println("Salary: "+e1.salary());
     }
 }
