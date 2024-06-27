f76b5ebc0d5fbf2e91459039255c651c1478d8cd
fsRakib <fsrakiba50@gmail.com>
2024-06-27 17:06:23 +0600

    Auto-sync commit

--- Creational/Factory/Mian.java
+++ Creational/Factory/Mian.java
@@ -6,5 +6,10 @@ public class Mian {
         System.out.println(employee);
         int salary = employee.salary();
         System.out.println("Salary: " + salary);
+
+        Employee employee2 = Factory.getEmployee("web");
+        System.out.println(employee2);
+        int salary2 = employee2.salary();
+        System.out.println("Salary: " + salary2);
     }
 }
