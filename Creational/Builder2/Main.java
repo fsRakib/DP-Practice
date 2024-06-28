10f6664eff8efbe0d3c0ad336ee746c5d57b8bad
fsRakib <fsrakiba50@gmail.com>
2024-06-28 21:26:23 +0600

    Auto-sync commit

--- Creational/Builder2/Main.java
+++ Creational/Builder2/Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Computer com = new Computer.ComputerBuilder()
                 .setProcessor("intel i7")
-                .setHdd(512)
                 .setRam(16)
+                .setHdd(512)
                 .build();
         System.out.println(com);
 
         Computer com2 = new Computer.ComputerBuilder()
                 .setProcessor("intel i9")
                 .setHdd(2024)
-                .setRam(68)
+               // .setRam(68)
                 .build();
         System.out.println(com2);
     }
