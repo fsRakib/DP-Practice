0977a22604e2cee867e4ef140804914b279bc80f
fsRakib <fsrakiba50@gmail.com>
2024-06-28 10:57:17 +0600

    Auto-sync commit

--- Structural/Composite/Main.java
+++ Structural/Composite/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Department salesDept = new SalesDept(1, "Sales Department");
         Department financeDept = new FinancialDept(2, "Financial Department");
-HeadDept headDept= new HeadDept(null, null)
+        HeadDept headDept = new HeadDept(3, "Head Department");
+
+        headDept.addDept(salesDept);
+        headDept.addDept(financeDept);
+
+        headDept.printDeptName();
     }
 }
