dee6bb976deeeea93b0cca03239d6bf9fd47bcca
fsRakib <fsrakiba50@gmail.com>
2024-06-28 11:42:12 +0600

    Auto-sync commit

--- Structural/Decorator2/SugarDecorator.java
+++ Structural/Decorator2/SugarDecorator.java
@@ -1,6 +1,13 @@
 package Structural.Decorator2;
 
-public class SugarDecorator extends CoffeeDecorator{
-    
-    
+public class SugarDecorator extends CoffeeDecorator {
+
+    public SugarDecorator(Coffee coffee) {
+        super(coffee);
+    }
+
+    @Override
+    public double getCost() {
+        return super.getCost() + 5;
+    }
 }
