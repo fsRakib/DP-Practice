6e21dc2e3ed130df0907d053b8729669241db6cc
fsRakib <fsrakiba50@gmail.com>
2024-06-28 21:01:26 +0600

    Auto-sync commit

---
+++ Creational/Builder2/Computer.java
@@ -0,0 +1,54 @@
+package Creational.Builder2;
+
+public class Computer {
+    // parameters
+    private String processor;
+    private int ram;
+    private int hdd;
+
+    // private constructor
+    public Computer() {
+    }
+
+    public String getProcessor() {
+        return processor;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public int getHdd() {
+        return hdd;
+    }
+
+    @Override
+    public String toString() {
+        return "Computer [processor=" + processor + ", ram=" + ram + ", hdd=" + hdd + "]";
+    }
+
+    public static class ComputerBuilder {
+
+        private String processor;
+        private int ram;
+        private int hdd;
+
+        // Setters for the builder
+        public void setProcessor(String processor) {
+            this.processor = processor;
+            return this;
+
+        }
+
+        public void setRam(int ram) {
+            this.ram = ram;
+            return this;
+        }
+
+        public void setHdd(int hdd) {
+            this.hdd = hdd;
+            return this;
+        }
+
+    }
+}
