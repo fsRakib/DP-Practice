00158682f5b8b2c99c382ee78e6ab49815b1a4ee
fsRakib <fsrakiba50@gmail.com>
2024-06-28 23:11:23 +0600

    Auto-sync commit

---
+++ Bahavioral/State2/Main.java
@@ -0,0 +1,11 @@
+package Bahavioral.State2;
+
+public class Main {
+    public static void main(String[] args) {
+        Light light = new Light();
+
+        // switching
+        light.switchState();
+        light.switchState();
+    }
+}
