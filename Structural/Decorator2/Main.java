4c05dee0b58d8675caac12de15476b2c1aa7d136
fsRakib <fsrakiba50@gmail.com>
2024-06-28 11:47:12 +0600

    Auto-sync commit

--- Structural/Decorator2/Main.java
+++ Structural/Decorator2/Main.java
@@ -2,9 +2,9 @@ package Structural.Decorator2;
 
 public class Main {
     public static void main(String[] args) {
-        Coffee coffee=new BaseCoffee();
-        coffee=new MilkDecorator(coffee);
-        coffee=new SugarDecorator(coffee);
+        Coffee coffee = new BaseCoffee();
+        coffee = new MilkDecorator(coffee);
+        coffee = new SugarDecorator(coffee);
         System.out.println(coffee.getCost());
     }
 }
