def59ca490fa3161649a405d3a4e26d3d9f56e7b
fsRakib <fsrakiba50@gmail.com>
2024-06-28 19:11:23 +0600

    Auto-sync commit

--- Creational/Factory2/ButtonFactory.java
+++ Creational/Factory2/ButtonFactory.java
@@ -1,5 +1,15 @@
 package Creational.Factory2;
 
+// Factory class to create buttons based on operating system
 public class ButtonFactory {
-   public static Button cre
+    // Method to create buttons
+    public static Button createButton(String osType) {
+        if ("Windows".equalsIgnoreCase(osType)) {
+            return new WindowsButton();
+        } else if ("Mac".equalsIgnoreCase(osType)) {
+            return new MacButton();
+        } else {
+            throw new IllegalArgumentException("Unknown OS type");
+        }
+    }
 }
