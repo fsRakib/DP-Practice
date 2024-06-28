fe6445a1bb6402445bce7757559ade41a6863192
fsRakib <fsrakiba50@gmail.com>
2024-06-28 17:46:23 +0600

    Auto-sync commit

--- Structural/Composite2/Main.java
+++ Structural/Composite2/Main.java
@@ -3,9 +3,9 @@ package Structural.Composite2;
 public class Main {
     public static void main(String[] args) {
         // create files
-        FileComponent file1 = new File("file1", 100);
-        FileComponent file2 = new File("file2", 200);
-        FileComponent file3 = new File("file3", 300);
+        FileComponent file1 = new File("file1.txt", 100);
+        FileComponent file2 = new File("file2.txt", 200);
+        FileComponent file3 = new File("file3.txt", 300);
 
         // create sub folder
         Folder subFolder = new Folder("Subfolder");
@@ -16,7 +16,7 @@ public class Main {
         mainFolder.add(file3);
         mainFolder.add(subFolder);
         mainFolder.add(file2);
-        
+
         System.out.println("Total size: " + mainFolder.getSize());
 
     }
