b0c1f3bb8306b14f2c706b2e47bf196803fc5086
fsRakib <fsrakiba50@gmail.com>
2024-06-29 02:52:12 +0600

    Auto-sync commit

--- Bahavioral/Chain_of_Responsibility/BankPaymentHandler.java
+++ Bahavioral/Chain_of_Responsibility/BankPaymentHandler.java
@@ -4,7 +4,6 @@ public class BankPaymentHandler extends PaymentHandler {
 
     @Override
     public void handlePayment(double amount) {
-        // TODO Auto-generated method stub
         if (amount < 500) {
             System.out.println("Payment of " + amount + " is handled by BankPaymentHandler");
         } else {
