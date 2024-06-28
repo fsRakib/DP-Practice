3c8248577ba11e546397a5373923f3079fed174c
fsRakib <fsrakiba50@gmail.com>
2024-06-28 19:21:23 +0600

    Auto-sync commit

--- Creational/Factory2/Main.java
+++ Creational/Factory2/Main.java
@@ -2,6 +2,10 @@ package Creational.Factory2;
 
 public class Main {
     public static void main(String[] args) {
-        Button winBtn = 
+        Button winBtn = ButtonFactory.createButton("windows");
+        winBtn.click();
+
+        Button macBtn = ButtonFactory.createButton("mac");
+        macBtn.click();
     }
 }
