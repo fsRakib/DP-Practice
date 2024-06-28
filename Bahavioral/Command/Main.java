917fab905a3a3e8952b53f880f649caa3bee5c52
fsRakib <fsrakiba50@gmail.com>
2024-06-29 00:26:23 +0600

    Auto-sync commit

--- Bahavioral/Command/Main.java
+++ Bahavioral/Command/Main.java
@@ -3,8 +3,11 @@ package Bahavioral.Command;
 public class Main {
     public static void main(String[] args) {
         FileOperationExecutor fileOperationExecutor = new FileOperationExecutor();
-        fileOperationExecutor.executeOperation(new OpenFile(new TextFile("file1.txt")));
-        fileOperationExecutor.executeOperation(new SaveFile(new TextFile("file2.txt")));
+
+        String openResult = fileOperationExecutor.executeOperation(new OpenFile(new TextFile("file1.txt")));
+        System.out.println(openResult); // Output: Opening file file1.txt
+
+        String saveResult = fileOperationExecutor.executeOperation(new SaveFile(new TextFile("file2.txt")));
+        System.out.println(saveResult); // Output: Saving file file2.txt
     }
 }
- 
