8a8d0bc61473eb1a13266edc0e2cc2e0bf101733
fsRakib <fsrakiba50@gmail.com>
2024-07-02 20:31:25 +0600

    Auto-sync commit

--- Creational/Prototype/Main.java
+++ Creational/Prototype/Main.java
@@ -5,11 +5,12 @@ public class Main {
         BookShop bs = new BookShop();
         bs.setShopName("Rokomari");
         bs.loadData();
+        bs.getBooks().remove(1);
         System.out.println(bs);
 
         BookShop bs2 = (BookShop)bs.clone();
-      //  bs2.setShopName("Aladin");
-        //bs2.loadData();
+        bs2.setShopName("Aladin.. ");
+        
         System.out.println(bs2);
     }
 }
