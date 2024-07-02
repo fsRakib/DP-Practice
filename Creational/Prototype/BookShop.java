e3771edbe23627bbcdc8760ea9c53b9573097132
fsRakib <fsrakiba50@gmail.com>
2024-07-02 20:11:25 +0600

    Auto-sync commit

---
+++ Creational/Prototype/BookShop.java
@@ -0,0 +1,31 @@
+package Creational.Prototype;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookShop {
+    private String shopName;
+    List<Book> books = new ArrayList<>();
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public void setShopName(String shopName) {
+        this.shopName = shopName;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "BookShop [shopName=" + shopName + ", books=" + books + "]";
+    }
+
+}
