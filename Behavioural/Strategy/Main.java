025b1d205e30c107aa13c5b560736f00d41a575a
fsRakib <fsrakiba50@gmail.com>
2024-07-04 23:21:23 +0600

    Auto-sync commit

---
+++ Behavioural/Strategy/Main.java
@@ -0,0 +1,10 @@
+package Behavioural.Strategy;
+
+public class Main {
+    public static void main(String[] args) {
+        Apply_Discount apply_Discount = new Apply_Discount(new CashBack_Discount());
+        apply_Discount.getDiscount();
+
+        apply_Discount.
+    }
+}
