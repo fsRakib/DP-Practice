aec5c7b8fec4c609672e71b4e21aff3e936bfe96
fsRakib <fsrakiba50@gmail.com>
2024-07-04 12:26:24 +0600

    Auto-sync commit

--- Creational/Factory/Main.java
+++ Creational/Factory/Main.java
@@ -3,9 +3,12 @@ package Creational.Factory;
 public class Main {
     public static void main(String[] args) {
         Employee e1 = Factory.getEmployee("android");
-        System.out.println(e1);
+
+        System.out.println(e1.salary());
 
         Employee e2 = Factory.getEmployee("web");
-        System.out.println(e2);
+
+        System.out.println(e2.salary());
+
     }
 }
