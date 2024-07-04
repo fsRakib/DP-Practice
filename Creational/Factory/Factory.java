3704f56f287e20c6c2687312245f1e3eedd97b69
fsRakib <fsrakiba50@gmail.com>
2024-07-04 12:21:24 +0600

    Auto-sync commit

--- Creational/Factory/Factory.java
+++ Creational/Factory/Factory.java
@@ -1,8 +1,14 @@
 package Creational.Factory;
 
 public class Factory {
-    //get employee
-    public static Employee getEmployee(String empType){
-        if()
+    // get employee
+    public static Employee getEmployee(String empType) {
+        if (empType.trim().equalsIgnoreCase("Android")) {
+            return new AndroidDev();
+        } else if (empType.trim().equalsIgnoreCase("Web")) {
+            return new WebDev();
+        } else {
+            return null;
+        }
     }
 }
--- Creational/Factory/Main.java
+++ Creational/Factory/Main.java
@@ -1,5 +1,11 @@
 package Creational.Factory;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Employee e1 = Factory.getEmployee("android");
+        System.out.println(e1);
+
+        Employee e2 = Factory.getEmployee("web");
+        System.out.println(e2);
+    }
 }
