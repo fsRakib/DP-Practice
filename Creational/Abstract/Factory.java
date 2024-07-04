3edd089a430666c67c456f882223283479330c2b
fsRakib <fsrakiba50@gmail.com>
2024-07-04 17:41:23 +0600

    Auto-sync commit

--- Creational/Abstract/Factory.java
+++ Creational/Abstract/Factory.java
@@ -3,6 +3,7 @@ package Creational.Abstract;
 public class Factory {
     // get employee
     public static Employee getEmployee(AbstructFactory absFactory) {
+        
         return absFactory.creatEmployee();
     }
 }
